6ee3fc34aa4d468322e9955545c412bf48f1ec0e
Alex <alexkrieg@gmx.ch>
2024-04-04 17:14:40 +0200

    -

--- unitTests/ExampleTest/main.cpp
+++ unitTests/ExampleTest/main.cpp
@@ -1,35 +1,11 @@
-#ifdef QT_ENABLED
-#include <QCoreApplication>
-#endif
 #include <iostream>
 #include "LibraryName.h"
 #include <iostream>
 #include "tests.h"
 
-#ifdef QT_WIDGETS_ENABLED
-#include <QWidget>
-#include <QApplication>
-#endif
-
-// Instantiate Tests here:
-// TEST_INSTANTIATE(Test_simple); // Where Test_simple is a derived class from the Test class
-TEST_INSTANTIATE(TST_simple); 
 
 int main(int argc, char* argv[])
 {
-#ifdef QT_WIDGETS_ENABLED
-	QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-	QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-	QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-#endif
-#ifdef QT_ENABLED
-	#ifdef QT_WIDGETS_ENABLED
-		QApplication app(argc, argv);
-	#else
-		QCoreApplication app(argc, argv);
-	#endif
-#endif
-
 	LibraryNamespace::LibraryInfo::printInfo();
 
 	std::cout << "Running "<< UnitTest::Test::getTests().size() << " tests...\n";
@@ -37,14 +13,5 @@ int main(int argc, char* argv[])
 	UnitTest::Test::runAllTests(results);
 	UnitTest::Test::printResults(results);
 
-#ifdef QT_WIDGETS_ENABLED
-	QWidget* widget = LibraryNamespace::LibraryInfo::createInfoWidget();
-	if (widget)
-		widget->show();
-#endif
-#ifdef QT_ENABLED
-	return app.exec();
-#else
-	return 0;
-#endif
+	return results.getSuccess();
 }
