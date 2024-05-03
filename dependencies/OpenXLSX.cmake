9cdac870caee41e313bfe53625e27f8a945ce5bb
Alex Krieg <alexkrieg@gmx.ch>
2024-05-03 09:04:20 +0200

    + OpenXLSX

---
+++ dependencies/OpenXLSX.cmake
@@ -0,0 +1,31 @@
+## description: OpenXLSX is a C++ library for reading, writing, creating and modifying Microsoft Excel® files, with the .xlsx format.
+
+# All credits go to the owner of the OpenXLSX repository
+function(dep SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
+    # Define the git repository and tag to download from
+    set(LIB_NAME OpenXLSX)								# Change this line
+    set(GIT_REPO https://github.com/troldal/OpenXLSX.git)	# Change this line
+    set(GIT_TAG master)										# Change this line
+
+    FetchContent_Declare(
+        ${LIB_NAME}
+        GIT_REPOSITORY ${GIT_REPO}
+        GIT_TAG        ${GIT_TAG}
+    )
+
+   
+    message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
+    FetchContent_MakeAvailable(${LIB_NAME})
+
+
+    # Add this library to the specific profiles of this project
+    list(APPEND DEPS_FOR_SHARED_LIB ${LIB_NAME}_static)
+    list(APPEND DEPS_FOR_STATIC_LIB ${LIB_NAME}_static)
+    list(APPEND DEPS_FOR_STATIC_PROFILE_LIB ${LIB_NAME}_static_profile) # only use for static profiling profile
+
+    set(${SHARED_LIB} "${${SHARED_LIB}};${DEPS_FOR_SHARED_LIB}" PARENT_SCOPE)
+    set(${STATIC_LIB} "${${STATIC_LIB}};${DEPS_FOR_STATIC_LIB}" PARENT_SCOPE)
+    set(${STATIC_PROFILE_LIB} "${${STATIC_PROFILE_LIB}};${DEPS_STATIC_PROFILE_LIB}" PARENT_SCOPE)
+endfunction()
+
+dep(DEPENDENCIES_FOR_SHARED_LIB DEPENDENCIES_FOR_STATIC_LIB DEPENDENCIES_FOR_STATIC_PROFILE_LIB)
