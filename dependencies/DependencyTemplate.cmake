fcb6bfa946474529e0f29abf06c5b0cdc978190a
Alex <alexkrieg@gmx.ch>
2024-09-13 11:19:23 +0200

    + include(FetchContent)

--- dependencies/DependencyTemplate.cmake
+++ dependencies/DependencyTemplate.cmake
@@ -1,4 +1,5 @@
 ## description: simple library to store application settings
+include(FetchContent)
 
 function(dep SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
     # Define the git repository and tag to download from
