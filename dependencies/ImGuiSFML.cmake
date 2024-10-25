d72953f8eccd59f0b8f331f32d2228b0ccf911a2
Alex <alexkrieg@gmx.ch>
2024-10-25 02:16:35 +0200

    + ImPlot for ImGui

--- dependencies/ImGuiSFML.cmake
+++ dependencies/ImGuiSFML.cmake
@@ -7,10 +7,17 @@ function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB INCLUDE
     set(GIT_REPO_1 https://github.com/ocornut/imgui.git)
     set(GIT_TAG_1 master)
 
-    set(LIB_NAME_2 imgui-sfml )
+    set(LIB_NAME_2 imgui-sfml)
 	set(LIB_MACRO_NAME_2 IMGUI_SFML_LIBRARY_AVAILABLE)
     set(GIT_REPO_2 https://github.com/KROIA/imgui-sfml-fork.git)
     set(GIT_TAG_2 2.6.x)
+
+    set(LIB_NAME_3 implot)
+	set(LIB_MACRO_NAME_3 IMPLOT_LIBRARY_AVAILABLE)
+    set(GIT_REPO_3 https://github.com/epezent/implot.git)
+    set(GIT_TAG_3 master)
+
+    
 
     FetchContent_Declare(
         ${LIB_NAME_1}
@@ -22,6 +29,13 @@ function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB INCLUDE
         GIT_REPOSITORY ${GIT_REPO_2}
         GIT_TAG        ${GIT_TAG_2}
     )
+    FetchContent_Declare(
+		${LIB_NAME_3}
+		GIT_REPOSITORY ${GIT_REPO_3}
+		GIT_TAG        ${GIT_TAG_3}
+	)
+
+
     message("Downloading dependency: ${LIB_NAME_1} from: ${GIT_REPO_1} tag: ${GIT_TAG_1}")
     FetchContent_MakeAvailable(${LIB_NAME_1})
 
@@ -34,14 +48,28 @@ function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB INCLUDE
     message("Downloading dependency: ${LIB_NAME_2} from: ${GIT_REPO_2} tag: ${GIT_TAG_2}")
     FetchContent_MakeAvailable(${LIB_NAME_2})
 
+    message("Downloading dependency: ${LIB_NAME_3} from: ${GIT_REPO_3} tag: ${GIT_TAG_3}")
+    FetchContent_MakeAvailable(${LIB_NAME_3})
+
+    set(IMPLOT_DIR ${implot_SOURCE_DIR})
+    # Add ImPlot to the project
+    add_library(implot STATIC
+        ${IMPLOT_DIR}/implot.cpp
+        ${IMPLOT_DIR}/implot_items.cpp
+    )
+    
+    target_include_directories(implot PUBLIC ${IMGUI_DIR})
+    target_include_directories(implot PUBLIC ${IMPLOT_DIR})
+    target_link_libraries(implot PUBLIC ImGui-SFML::ImGui-SFML)
+
     
     # Add this library to the specific profiles of this project
-    list(APPEND DEPS_FOR_SHARED_LIB ImGui-SFML::ImGui-SFML)
-    list(APPEND DEPS_FOR_STATIC_LIB ImGui-SFML::ImGui-SFML)
-    list(APPEND DEPS_FOR_STATIC_PROFILE_LIB ImGui-SFML::ImGui-SFML) # only use for static profiling profile
-    list(APPEND INCS ${IMGUI_DIR} ) 
+    list(APPEND DEPS_FOR_SHARED_LIB ImGui-SFML::ImGui-SFML implot)
+    list(APPEND DEPS_FOR_STATIC_LIB ImGui-SFML::ImGui-SFML implot)
+    list(APPEND DEPS_FOR_STATIC_PROFILE_LIB ImGui-SFML::ImGui-SFML implot) # only use for static profiling profile
+    list(APPEND INCS ${IMGUI_DIR} ${IMPLOT_DIR}) 
 
-	set(${LIBRARY_MACRO_NAME} "${${LIBRARY_MACRO_NAME}};${LIB_MACRO_NAME_1};${LIB_MACRO_NAME_2}" PARENT_SCOPE)
+	set(${LIBRARY_MACRO_NAME} "${${LIBRARY_MACRO_NAME}};${LIB_MACRO_NAME_1};${LIB_MACRO_NAME_2};${LIB_MACRO_NAME_3}" PARENT_SCOPE)
     set(${SHARED_LIB} "${${SHARED_LIB}};${DEPS_FOR_SHARED_LIB}" PARENT_SCOPE)
     set(${STATIC_LIB} "${${STATIC_LIB}};${DEPS_FOR_STATIC_LIB}" PARENT_SCOPE)
     set(${STATIC_PROFILE_LIB} "${${STATIC_PROFILE_LIB}};${DEPS_FOR_STATIC_PROFILE_LIB}" PARENT_SCOPE)
