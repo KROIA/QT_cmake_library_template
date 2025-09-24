c1ffc5c4012487a24ce168550fde55a410c0cffa
Alex Krieg <alexkrieg@gmx.ch>
2025-09-24 16:36:58 +0200

    v1.5.1
    + helper macros:
      + downloadStandardLibrary
      + downloadExternalLibrary

--- dependencies/DependencyTemplate.cmake
+++ dependencies/DependencyTemplate.cmake
@@ -7,49 +7,11 @@ function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB INCLUDE
     set(LIB_MACRO_NAME APP_SETTINGS_LIBRARY_AVAILABLE)		# Change this line
     set(GIT_REPO https://github.com/KROIA/AppSettings.git)	# Change this line
     set(GIT_TAG main)										# Change this line
+    set(NO_EXAMPLES True)	
+    set(NO_UNITTESTS True)
+    set(ADDITIONAL_INCLUDE_PATHS )
 
-    FetchContent_Declare(
-        ${LIB_NAME}
-        GIT_REPOSITORY ${GIT_REPO}
-        GIT_TAG        ${GIT_TAG}
-    )
-
-    set(${LIB_NAME}_NO_EXAMPLES True)						# Disables the examlpes of the library
-    set(${LIB_NAME}_NO_UNITTTESTS True)						# Disables the unittests of the library
-    
-    # Check if the library has already been populated
-    FetchContent_GetProperties(${LIB_NAME})
-    if(NOT ${LIB_NAME}_ALREADY_POPULATED OR NOT ${LIB_NAME}_SOURCE_DIR OR NOT ${LIB_NAME}_BINARY_DIR)
-        message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
-        FetchContent_MakeAvailable(${LIB_NAME})
-        # Set a persistent cache variable to mark the library as populated
-        set(${LIB_NAME}_ALREADY_POPULATED TRUE CACHE INTERNAL "Mark ${LIB_NAME} as populated")
-    else()
-        # Re-run MyLibrary's CMakeLists.txt to set up include dirs, libraries, etc.
-        add_subdirectory("${${LIB_NAME}_SOURCE_DIR}" "${${LIB_NAME}_BINARY_DIR}" EXCLUDE_FROM_ALL)
-    endif()
-
-    set(${LIB_NAME}_NO_EXAMPLES True)						# Disables the examlpes of the library
-    set(${LIB_NAME}_NO_UNITTTESTS True)						# Disables the unittests of the library
-
-    message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
-    FetchContent_MakeAvailable(${LIB_NAME})
-
-    # Add this library to the specific profiles of this project
-    list(APPEND DEPS_FOR_SHARED_LIB ${LIB_NAME}_shared)
-    list(APPEND DEPS_FOR_STATIC_LIB ${LIB_NAME}_static)
-    list(APPEND DEPS_FOR_STATIC_PROFILE_LIB ${LIB_NAME}_static_profile) # only use for static profiling profile
-
-    # Add additional include paths
-    #  string(TOLOWER ${LIB_NAME} LIB_NAME_LOWER)
-    #  list(APPEND INCLUDES ${${LIB_NAME_LOWER}_SOURCE_DIR}/include)
-    list(APPEND INCLUDES )
-
-    set(${LIBRARY_MACRO_NAME} "${${LIBRARY_MACRO_NAME}};${LIB_MACRO_NAME}" PARENT_SCOPE)
-    set(${SHARED_LIB} "${${SHARED_LIB}};${DEPS_FOR_SHARED_LIB}" PARENT_SCOPE)
-    set(${STATIC_LIB} "${${STATIC_LIB}};${DEPS_FOR_STATIC_LIB}" PARENT_SCOPE)
-    set(${STATIC_PROFILE_LIB} "${${STATIC_PROFILE_LIB}};${DEPS_FOR_STATIC_PROFILE_LIB}" PARENT_SCOPE)
-    set(${INCLUDE_PATHS} "${${INCLUDE_PATHS}};${INCLUDES}" PARENT_SCOPE)
+    downloadStandardLibrary()
 endfunction()
 
 dep(DEPENDENCY_NAME_MACRO
--- unitTests/UnitTest.cmake
+++ unitTests/UnitTest.cmake
@@ -1,23 +1,16 @@
 ## description: simple library create unit tests
-include(FetchContent)
 
-# Define the git repository and tag to download from
-set(LIB_NAME UnitTest)								
-set(GIT_REPO https://github.com/KROIA/UnitTest.git)	
-set(GIT_TAG main)									
+function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
+    # Define the git repository and tag to download from
+    set(LIB_NAME UnitTest)		
+    set(LIB_MACRO_NAME UNIT_TEST_LIBRARY_AVAILABLE)
+    set(GIT_REPO https://github.com/KROIA/UnitTest.git)	
+    set(GIT_TAG main)		
+	set(NO_EXAMPLES True)	
+	set(NO_UNITTESTS True)
+	set(ADDITIONAL_INCLUDE_PATHS )
 
-FetchContent_Declare(
-    ${LIB_NAME}
-    GIT_REPOSITORY ${GIT_REPO}
-    GIT_TAG        ${GIT_TAG}
-)
+	downloadStandardLibrary()
+endfunction()
 
-set(${LIB_NAME}_NO_EXAMPLES True)						# Disables the examlpes of the library
-set(${LIB_NAME}_NO_UNITTESTS True)						# Disables the unittests of the library
-message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
-FetchContent_MakeAvailable(${LIB_NAME})
-
-# Add this library to the specific profiles of this project
-list(APPEND DEPENDENCIES_FOR_SHARED_LIB ${LIB_NAME}_static)
-list(APPEND DEPENDENCIES_FOR_STATIC_LIB ${LIB_NAME}_static)
-list(APPEND DEPENDENCIES_FOR_STATIC_PROFILE_LIB ${LIB_NAME}_static_profile) # only use for static profiling profile
+dep(DEPENDENCY_NAME_MACRO DEPENDENCIES_FOR_SHARED_LIB DEPENDENCIES_FOR_STATIC_LIB DEPENDENCIES_FOR_STATIC_PROFILE_LIB)
