fc9d86d95c0e5741c4831709745418d4299646d1
Alex Krieg <alexkrieg@gmx.ch>
2025-09-24 18:28:27 +0200

    -

--- dependencies/QSFML_EditorWidget.cmake
+++ dependencies/QSFML_EditorWidget.cmake
@@ -1,6 +1,6 @@
 ## description: SFML based Game Engine designed to use in Qt applications
 
-function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
+function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB INCLUDE_PATHS)
     # Define the git repository and tag to download from
     set(LIB_NAME QSFML_EditorWidget)
     set(LIB_MACRO_NAME QSFML_EDITOR_WIDGET_LIBRARY_AVAILABLE)
