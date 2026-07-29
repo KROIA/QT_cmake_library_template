57f39b40e949dd6ad389d624e311f61c38998aa8
Alex Krieg <alexkrieg@gmx.ch>
2026-07-29 17:39:09 +0200

    ~ EASTL.cmake

--- dependencies/EASTL.cmake
+++ dependencies/EASTL.cmake
@@ -4,8 +4,12 @@ include(FetchContent)
 function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB INCLUDE_PATHS)
     set(LIB_NAME EASTL)
 	set(LIB_MACRO_NAME EASTL_LIBRARY_AVAILABLE)
-    set(GIT_REPO https://github.com/KROIA/EASTL.git)
+    set(GIT_REPO https://github.com/electronicarts/EASTL.git)
     set(GIT_TAG master)
+
+    # Allow old CMakeLists.txt to work: suppress the version-too-old error by telling CMake
+    # to interpret cmake_minimum_required(VERSION 3.5) as if it was cmake_minimum_required(VERSION 3.20)
+    set(CMAKE_POLICY_VERSION_MINIMUM 3.5)
 
     downloadExternalLibrary()
 endfunction()
