000afc7cbeddbedc93b16f986a1e729aa54d2f9c
Alex <alexkrieg@gmx.ch>
2026-08-05 18:08:36 +0200

    ~ DomainDrivenDesign cmake name

---
+++ dependencies/DDD.cmake
@@ -0,0 +1,38 @@
+## description: Basic Domain Driven Design pattern implementation in C++
+include(FetchContent)
+
+function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
+    # Define the git repository and tag to download from
+    set(LIB_NAME DDD)
+    set(LIB_MACRO_NAME DDD_LIBRARY_AVAILABLE)
+    set(GIT_REPO https://github.com/KROIA/DomainDrivenDesign.git)
+    set(GIT_TAG main)
+
+    FetchContent_Declare(
+        ${LIB_NAME}
+        GIT_REPOSITORY ${GIT_REPO}
+        GIT_TAG        ${GIT_TAG}
+    )
+
+    set(${LIB_NAME}_NO_EXAMPLES True)
+    set(${LIB_NAME}_NO_UNITTESTS True)
+    
+    # Check if already populated
+    FetchContent_GetProperties(${LIB_NAME})
+    if(NOT ${LIB_NAME}_POPULATED)
+        message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
+        FetchContent_MakeAvailable(${LIB_NAME})
+    endif()
+
+    # Add this library to the specific profiles of this project
+    list(APPEND DEPS_FOR_SHARED_LIB ${LIB_NAME}_shared)
+    list(APPEND DEPS_FOR_STATIC_LIB ${LIB_NAME}_static)
+    list(APPEND DEPS_FOR_STATIC_PROFILE_LIB ${LIB_NAME}_static_profile) # only use for static profiling profile
+
+	set(${LIBRARY_MACRO_NAME} "${${LIBRARY_MACRO_NAME}};${LIB_MACRO_NAME}" PARENT_SCOPE)
+    set(${SHARED_LIB} "${${SHARED_LIB}};${DEPS_FOR_SHARED_LIB}" PARENT_SCOPE)
+    set(${STATIC_LIB} "${${STATIC_LIB}};${DEPS_FOR_STATIC_LIB}" PARENT_SCOPE)
+    set(${STATIC_PROFILE_LIB} "${${STATIC_PROFILE_LIB}};${DEPS_FOR_STATIC_PROFILE_LIB}" PARENT_SCOPE)
+endfunction()
+
+dep(DEPENDENCY_NAME_MACRO DEPENDENCIES_FOR_SHARED_LIB DEPENDENCIES_FOR_STATIC_LIB DEPENDENCIES_FOR_STATIC_PROFILE_LIB)
